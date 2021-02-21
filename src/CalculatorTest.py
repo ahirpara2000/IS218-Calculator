5e020d8954ede97c81b90565294509af3a71b78c
ahirpara2000 <ahirpara2000@gmail.com>
2021-02-21 14:30:21 -0500

    Task: added comments

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -4,30 +4,37 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    # 1. Check calculator instance
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    # 2. Check result property
     def test_result_property_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.result, 0)
 
+    # 3. Check addition method
     def test_add_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
 
+    # 4. Check subtraction method
     def test_subtract_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.subtract(2, 2), 0)
 
+    # 5. Check multiplication method
     def test_multiplication_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.multiply(3, 2), 6)
 
+    # 6. Check division method
     def test_division_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.divide(3, 2), 1.5)
 
+    # 7. Check square method
     def test_square_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.square(3), 9)
