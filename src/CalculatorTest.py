27a276e1dac8604784dd34a2830c8e07c5e25482
ahirpara2000 <ahirpara2000@gmail.com>
2021-02-21 14:06:45 -0500

    Feature: setup calculator with subtraction method

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -10,11 +10,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_result_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
+        self.assertEqual(calculator.subtract(2, 2), 0)
 
 
 if __name__ == '__main__':
